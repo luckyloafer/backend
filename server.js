8178c433bba8aa9c93a38cd508eefbf5fc719b8a
luckyloafer <patnanalakshmanamurthy9@gmail.com>
2024-09-22 15:57:19 +0530

    test4

--- server.js
+++ server.js
@@ -19,21 +19,41 @@ const io = socketIo(server, {
 
 // Socket.io connection
 const onlineUsers = new Set();
-io.on("connection", (socket) => {
-  console.log("A user connected");
-  socket.on("registerUser", (userId) => {
-    onlineUsers.add(userId); // Add the userId to the Set
-    io.emit("onlineUsers", onlineUsers.size); // Emit the unique user count to all clients
-  });
-  socket.on("message", (message) => {
-    console.log("Message received:", message);
-    io.emit("message", message); // Broadcast the message to all clients
-  });
+// io.on("connection", (socket) => {
+//   console.log("A user connected");
+//   socket.on("registerUser", (userId) => {
+//     onlineUsers.add(userId); // Add the userId to the Set
+//     io.emit("onlineUsers", onlineUsers.size); // Emit the unique user count to all clients
+//   });
+//   socket.on("message", (message) => {
+//     console.log("Message received:", message);
+//     io.emit("message", message); // Broadcast the message to all clients
+//   });
 
-  socket.on("disconnect", () => {
-    io.emit('onlineUsers', onlineUsers.size);
+//   socket.on("disconnect", () => {
+//     io.emit('onlineUsers', onlineUsers.size);
+//   });
+// });
+
+io.on("connection", (socket) => {
+    console.log("A user connected", socket.id);
+  
+    socket.on("registerUser", (userId) => {
+      // You could store the mapping of socket.id to userId in an object
+      onlineUsers.add(socket.id); // Add socket id to track the connection
+      io.emit("onlineUsers", onlineUsers.size); // Emit updated online user count
+    });
+  
+    socket.on("disconnect", () => {
+      onlineUsers.delete(socket.id); // Remove user based on socket id
+      io.emit("onlineUsers", onlineUsers.size); // Emit updated user count
+    });
+
+       socket.on("message", (message) => {
+    console.log("Message received:", message);
+     io.emit("message", message); // Broadcast the message to all clients
+   });
   });
-});
 
 // Basic route for testing
 app.get("/", (req, res) => {
