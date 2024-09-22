e889c0aa9a904d5d8d82a31d31802e02c25f88e8
luckyloafer <patnanalakshmanamurthy9@gmail.com>
2024-09-22 15:29:51 +0530

    online users test1

--- server.js
+++ server.js
@@ -1,46 +1,48 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const cors = require('cors');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
 const io = socketIo(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST'],
-        credentials: true
-      },
-      transports: ['websocket', 'polling'],  // Ensure both WebSocket and polling are supported
-      allowEIO3: true 
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+  transports: ["websocket", "polling"], // Ensure both WebSocket and polling are supported
+  allowEIO3: true,
 });
 
 // Socket.io connection
-let onlineUsers = 0;
-io.on('connection', (socket) => {
-    console.log('A user connected');
-    onlineUsers++;
-    io.emit('onlineUsers', onlineUsers);
-    socket.on('message', (message) => {
-        console.log('Message received:', message);
-        io.emit('message', message); // Broadcast the message to all clients
-    });
+const onlineUsers = new Set();
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.on("registerUser", (userId) => {
+    onlineUsers.add(userId); // Add the userId to the Set
+    io.emit("onlineUsers", onlineUsers.size); // Emit the unique user count to all clients
+  });
+  socket.on("message", (message) => {
+    console.log("Message received:", message);
+    io.emit("message", message); // Broadcast the message to all clients
+  });
 
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
+  socket.on("disconnect", () => {
+    io.emit('onlineUsers', onlineUsers.size);
+  });
 });
 
 // Basic route for testing
-app.get('/', (req, res) => {
-    res.send('Socket.io Server is running');
+app.get("/", (req, res) => {
+  res.send("Socket.io Server is running");
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
